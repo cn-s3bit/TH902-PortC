4f3a0d645884414ab33f5339c0835a0f99581686
?????·è?? <shiruoxi2010@163.com>
2019-02-10 16:14:50 +0800

    Render Manager Implementation

--- Game/GameContents/RenderManager.c
+++ Game/GameContents/RenderManager.c
@@ -1,1 +1,31 @@
 #include "RenderManager.h"
+
+ArrayList * layers[RENDER_LAYER_MAX];
+Deque * layer_empty_ids[RENDER_LAYER_MAX];
+
+void initialize_layer(int layerId) {
+	if (layer_empty_ids[layerId] == NULL) {
+		layers[layerId] = create_array_list(sizeof(Renderable *), 32u);
+		layer_empty_ids[layerId] = create_deque(sizeof(int), 32u);
+	}
+}
+
+void register_renderable(Renderable * renderable) {
+	initialize_layer(renderable->Layer);
+	if (get_deque_size(layer_empty_ids[renderable->Layer]) > 0) {
+		unsigned int index;
+		poll_deque_tail(layer_empty_ids[renderable->Layer], &index);
+		assign_array_list_element(layers[renderable->Layer], index, &renderable);
+		renderable->_internalId = index;
+	}
+	else {
+		renderable->_internalId = layers[renderable->Layer]->Size;
+		append_array_list(layers[renderable->Layer], &renderable);
+	}
+}
+
+void unregister_renderable(Renderable * renderable) {
+	Renderable * null_handler = NULL;
+	assign_array_list_element(layers[renderable->Layer], renderable->_internalId, &null_handler);
+	push_deque_tail(layer_empty_ids[renderable->Layer], &renderable->_internalId);
+}
--- Game/GameContents/RenderManager.h
+++ Game/GameContents/RenderManager.h
@@ -2,34 +2,41 @@
 #define RENDER_MANAGER_H
 #include "../SDLEx/SDLWithPlugins.h"
 #include "../SDLEx/MathEx/MathEx.h"
+#include "../SDLEx/Utils/Deque.h"
 enum RenderLayer {
 	RENDER_LAYER_BACKGROUND = 1,  // Background
-	RENDER_LAYER_ENTITY_0 = 10,  // Bullet of Player 1
-	RENDER_LAYER_ENTITY_1 = 20,  // Bullet of Player 2
-	RENDER_LAYER_ENTITY_2 = 30,  // Player & Player Wing
-	RENDER_LAYER_ENTITY_3 = 40,  // NPC, except Boss
-	RENDER_LAYER_ENTITY_4 = 50,  // Enemy Bullets Lower Layer 1
-	RENDER_LAYER_ENTITY_5 = 60,  // Enemy Bullets Lower Layer 2
-	RENDER_LAYER_ENTITY_6 = 70,  // Enemy Bullets Additive
-	RENDER_LAYER_ENTITY_7 = 80,  // Enemy Bullets High Layer
-	RENDER_LAYER_ENTITY_8 = 90,  // Boss
-	RENDER_LAYER_ENTITY_9 = 100,  // Particles
-	RENDER_LAYER_UI_0 = 201,  // UI 1
-	RENDER_LAYER_UI_1 = 202,  // UI 2
-	RENDER_LAYER_UI_2 = 203,  // UI 3
-	RENDER_LAYER_MAX = 255,  // Array Length
+	RENDER_LAYER_ENTITY_0 = 4,  // Bullet of Player 1
+	RENDER_LAYER_ENTITY_1 = 8,  // Bullet of Player 2
+	RENDER_LAYER_ENTITY_2 = 12,  // Player & Player Wing
+	RENDER_LAYER_ENTITY_3 = 16,  // NPC, except Boss
+	RENDER_LAYER_ENTITY_4 = 20,  // Enemy Bullets Lower Layer 1
+	RENDER_LAYER_ENTITY_5 = 24,  // Enemy Bullets Lower Layer 2
+	RENDER_LAYER_ENTITY_6 = 28,  // Enemy Bullets Additive
+	RENDER_LAYER_ENTITY_7 = 32,  // Enemy Bullets High Layer
+	RENDER_LAYER_ENTITY_8 = 36,  // Boss
+	RENDER_LAYER_ENTITY_9 = 40,  // Particles
+	RENDER_LAYER_UI_0 = 51,  // UI 1
+	RENDER_LAYER_UI_1 = 52,  // UI 2
+	RENDER_LAYER_UI_2 = 53,  // UI 3
+	RENDER_LAYER_MAX = 64,  // Array Length
 };
 typedef struct TextureRegion {
 	long TextureID;
 	SDL_Rect Rect;
 } TextureRegion;
 typedef struct Renderable {
-	enum RenderLayer Layer;
 	TextureRegion TextureRegion;
+	// _internalId should not be changed as long as the Renderable is registered
+	long long _internalId;
+	// Layer should not be changed as long as the Renderable is registered
+	enum RenderLayer Layer;
 	Vector4 Color;
 	Vector2 Center;
 	float Rotation;
 	Vector2 Scale;
 } Renderable;
+
+void register_renderable(Renderable * renderable);
+void unregister_renderable(Renderable * renderable);
 
 #endif
