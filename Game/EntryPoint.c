b34e612ccc47065e4ac73d9c20eb020fa5b2bc71
北海若 <shiruoxi2010@163.com>
2019-02-10 10:50:46 +0800

    Add Game Icon

--- Game/EntryPoint.c
+++ Game/EntryPoint.c
@@ -40,7 +40,8 @@ int main(int argc, char ** argv) {
 		SDL_Log("Working Path: %s\n", argv[0]);
 	init_sdl();
 	SDL_Window * window = SDL_CreateWindow("TH902", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 960, 720, SDL_WINDOW_VULKAN);
-
+	SDL_Surface * icon = IMG_Load(RESOURCE_FOLDER "Game/Image/icon32.png");
+	SDL_SetWindowIcon(window, icon);
 	TTF_Font * testFont = TTF_OpenFont(DEFAULT_FONT_PATH, 32);
 
 	initialize_vulkan(window, VK_MAKE_VERSION(0, 1, 0));
