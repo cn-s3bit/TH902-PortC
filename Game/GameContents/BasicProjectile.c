ada27ae44488c5f6bd2ffdf3d8d3af9eacc17fbd
北海若 <shiruoxi2010@163.com>
2019-02-11 17:02:51 +0800

    Projectile Type Descriptor

--- Game/GameContents/BasicProjectile.c
+++ Game/GameContents/BasicProjectile.c
@@ -1,12 +1,31 @@
 #include "BasicProjectile.h"
 
 #define MAX_PROJ 8192
+#define MAX_PROJ_TYPE 256
 
 Deque * free_projectile_ids;
 Projectile projectiles[MAX_PROJ];
+ProjectileTypeDescriptor projectileTypes[MAX_PROJ_TYPE];
+
+inline void add_type(short type, int x, int y, int w, int h, float originX, float originY, float pdr0, int colorId) {
+	projectileTypes[type].Region.x = x;
+	projectileTypes[type].Region.y = y;
+	projectileTypes[type].Region.w = w;
+	projectileTypes[type].Region.h = h;
+	projectileTypes[type].Origin.X = originX;
+	projectileTypes[type].Origin.Y = originY;
+	projectileTypes[type].JudgeRadius = pdr0;
+	projectileTypes[type].ColorID = colorId;
+}
+
+inline void add_types() {
+	add_type(1, 0, 0, 12, 16, 6, 8, 2, 0);
+	add_type(2, 20, 0, 12, 16, 6, 8, 2, 1);
+}
 
 inline void initialize_globals() {
 	if (free_projectile_ids == NULL) {
+		add_types();
 		free_projectile_ids = create_deque(sizeof(int), 128u);
 		for (int i = MAX_PROJ - 1; i >= 0; i--) {
 			push_deque_tail(free_projectile_ids, &i);
@@ -54,3 +73,33 @@ void update_projectiles() {
 		}
 	}
 }
+
+const int bulletTypes[25][8] = {
+	{ 1,2,3,4,5,6,7,8 }, // 0
+	{ 9,10,11,12,13,14,15,16 }, // 1
+	{ 17,18,19,20,21,22,23,24 }, // 2
+	{ 25,26,27,28,29,30,31,32 }, // 3
+	{ 33,34,35,36,37,38,39,40 }, // 4
+	{ 41,42,43,44,45,46,47,48 }, // 5
+	{ 73,74,75,76,77,78,79,80 }, // 6
+	{ 49,50,51,52,53,54,55,56 }, // 7
+	{ 57,58,59,60,61,62,63,64 }, // 8
+	{ 65,66,67,68,69,70,71,72 }, // 9
+	{ 81,82,83,84,85,86,87,88 }, // 10
+	{ 89,90,91,92,93,94,95,96 }, // 11
+	{ 136,137,138,139,140,141,142 }, // 12
+	{ 97,98,99,100,101,102,103 }, // 13
+	{ 104,105,106,107,108,109,110,111 }, // 14
+	{ 112,113,114,115,116,117,118,119 }, // 15
+	{ 120,121,122,123,124,125,126,127 }, // 16
+	{ 128,129,130,131,132,133,134,135 }, // 17
+	{ 143,144,145,146,147,148,149,150 }, // 18
+	{ 151,152,153,154,155,156,157,158 }, // 19
+	{ 159,160,161,162,163,164,165,166 }, // 20
+
+	{ 167,168,169,170,171,172,173 }, // 21
+
+	{ 203,204,205,206,207,208,209,210 }, // 22
+	{ 210,211,212,213,214,215,216,217 }, // 23
+	{ 221,222,223,224,225,226,227,228 } // 24
+};
--- Game/GameContents/BasicProjectile.h
+++ Game/GameContents/BasicProjectile.h
@@ -5,15 +5,27 @@
 
 typedef struct Projectile {
 	short Active;
+	short type;
 	int WhoAmI;
 	Vector2 Position;  // Center
 	Vector2 Velocity;
 	Vector2 Accel;
 	Renderable * RenderablePt;
+	void * CustomData;
 	void (*AI)(void * proj);
 } Projectile;
 
+typedef struct ProjectileTypeDescriptor {
+	SDL_Rect Region;
+	Vector2 Origin;
+	float JudgeRadius;
+	short ColorID;
+} ProjectileTypeDescriptor;
+
+
 void update_basic_projectile(Projectile * proj);
 Projectile * alloc_projectile();
+void free_projectile(Projectile * proj);
+void update_projectiles();
 
 #endif
