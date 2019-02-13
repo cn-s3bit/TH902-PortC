9e9ead0107101f6edd6b356574bf6ef00808b8ba
北海若 <shiruoxi2010@163.com>
2019-02-13 19:48:32 +0800

    Test AI

--- Game/EntryPoint.c
+++ Game/EntryPoint.c
@@ -7,6 +7,8 @@
 #include "SDLEx/MathEx/MathEx.h"
 #include "Constants.h"
 #include "SDLEx/Vulkan/SDLExVulkan.h"
+#include "GameContents/BasicProjectile.h"
+#include "GameContents/ResourceManager.h"
 
 #include "SDLEx/Utils/HashMap.h"
 
@@ -35,6 +37,11 @@ short handle_event(void) {
 	return NO_SIGNAL;
 }
 
+void test_ai0(Projectile * proj) {
+	SDL_Log("%d %f", proj->WhoAmI, proj->Accel.Y);
+	proj->Accel = vector2_scl((vector2_unit(vector2_create(rand() - RAND_MAX / 2, rand() - RAND_MAX / 2))), 0.03f);
+}
+
 int main(int argc, char ** argv) {
 	if (argc > 0)
 		SDL_Log("Working Path: %s\n", argv[0]);
@@ -50,27 +57,27 @@ int main(int argc, char ** argv) {
 	initialize_vulkan(window, VK_MAKE_VERSION(0, 1, 0));
 	create_graphics_pipeline_f(RESOURCE_FOLDER "Shaders/default.vert.spv", RESOURCE_FOLDER "Shaders/default.frag.spv");
 	long texture_id = load_texture2d(RESOURCE_FOLDER "Game/Image/Barrages.png");
-	bind_all_images_texture2d(texture_id);
+	// bind_all_images_texture2d(texture_id);
+	resources.Images.Barrages = texture_id;
 	int t = 0;
+	Projectile * projs[8];
+	for (int i = 0; i < 8; i++) {
+		projs[i] = alloc_projectile();
+		projs[i]->Type = rand() % 7 + 8;
+		projs[i]->Position = vector2_create(480.0f, 360.0f);
+		projs[i]->AI = test_ai0;
+		raii_projectile_renderable(projs[i]);
+	}
 	// Main Loop
 	while (1) {
 		t++;
 		if (handle_event() == EXIT_SIGNAL)
 			goto LABEL_EXIT;
 		clock_t b = clock();
+		sdlex_set_blend_mode(SDLEX_BLEND_MODE_ADDITIVE);
+		update_projectiles();
 		unsigned imageid = sdlex_begin_frame();
-		for (int i = 0; i < SDL_max(200 - t / 3, 1); i++) {
-			SDL_Rect p1 = { 77, -28, 24, 24 };
-			SDL_Rect p2 = { 0, 0, 24, 24 };
-			p1.x += t + i;
-			sdlex_set_blend_mode(SDLEX_BLEND_MODE_ADDITIVE);
-			sdlex_render_texture(imageid, p1);
-			sdlex_render_texture_region_ex(imageid,
-				vector2_add(vector2_rotate(vector2_scl(vector2_unitX(), 90), (float)(12 * i + t)), vector2_scl(vector2_one(), (float)t)),
-				vector2_adds(vector2_zero(), 12, 12),
-				1.0f * (float)(t + i),
-				vector2_scl(vector2_one(), 1.0f), vector4_create(1.0f, 1.0f, 1.0f, 0.3f), p2);
-		}
+		render_all_layers(imageid);
 		sdlex_end_frame(imageid);
 		SDL_Log("%d", clock() - b);
 		SDL_Delay(SDL_max(16 - clock() + b, 0));
