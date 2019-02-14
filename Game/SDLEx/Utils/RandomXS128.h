c598930e01531b2f7c46a15a9433be79a6f98e97
ALINGCAT <2218773842@qq.com>
2019-02-14 11:44:56 +0800

    Add RandomXS128.h

---
+++ Game/SDLEx/Utils/RandomXS128.h
@@ -0,0 +1,26 @@
+#include "../SDLWithPlugins.h"
+#ifndef SDLEX_RANDOMXS128_H
+#define SDLEX_RANDOMXS128_H
+#define NORM_DOUBLE (1.0/(1LL << 53))
+#define NORM_FLOAT (1.0/(1LL << 24))
+typedef struct RandomXS128 {
+	long long seed0;
+	long long seed1;
+} RandomXS128;
+
+RandomXS128 * randomxs_create_no_seed();
+RandomXS128 * randomxs_create_a_seed(long long seed);
+RandomXS128 * randomxs_create(long long seed0, long long seed1);
+
+long long randomxs_next_long();
+long long randomxs_next_long_limited(long long n); //@param n the positive bound on the random number to be returned. n -> final
+long randomxs_next(long bits); //protected final
+long randomxs_next_int();
+long randomxs_next_int_limited(long n); //n -> final
+double randomxs_next_double();
+double randomxs_next_float();
+SDL_bool randomxs_next_boolean();
+
+#endif // !SDLEX_RANDOMXS128_H
+
+/*实现成SDLEx/Utils/RandomXS128.h和SDLEx/Utils/RandomXS128.c 所有成员函数第一个参数放RandomXS128 * thiz，函数名加前缀randomxs_ 实现结构可以参考ArrayList*/
