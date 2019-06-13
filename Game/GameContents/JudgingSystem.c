a4418bcfa55b026ae4ed1335c84ef2762e21b8c1
北海若 <shiruoxi2010@163.com>
2019-06-13 15:04:26 +0800

    Judge System Logic

--- Game/GameContents/JudgingSystem.c
+++ Game/GameContents/JudgingSystem.c
@@ -1,1 +1,50 @@
 #include "JudgingSystem.h"
+
+#define MAX_COLLIDERS 8102
+
+Collider * colliders[COLLIDER_CLASSES][MAX_COLLIDERS];
+Deque * empty_ids[COLLIDER_CLASSES];
+
+void register_judge(Collider * collider) {
+	if (empty_ids[collider->Class] == NULL) {
+		empty_ids[collider->Class] = create_deque(sizeof(int), 32u);
+		for (int i = 0; i < MAX_COLLIDERS; i++) {
+			push_deque_tail(empty_ids[collider->Class], &i);
+		}
+	}
+	int index = 0;
+	poll_deque_head(empty_ids[collider->Class], &index);
+	colliders[collider->Class][index] = collider;
+	collider->_internalId = index;
+}
+
+void unregister_judge(Collider * collider) {
+	colliders[collider->Class][collider->_internalId] = NULL;
+	push_deque_tail(empty_ids[collider->Class], &collider->_internalId);
+}
+
+void judge_hits() {
+	for (int firstLayer = 0; firstLayer < COLLIDER_CLASSES; firstLayer++) {
+		for (int firstIndex = 0; firstIndex < MAX_COLLIDERS; firstIndex++) {
+			if (colliders[firstLayer][firstIndex] != NULL) {
+				short mask = colliders[firstLayer][firstIndex]->CollideMask;
+				for (int secondLayer = 0; secondLayer < COLLIDER_CLASSES; secondLayer++) {
+					if ((mask & secondLayer) == 0) continue;
+					for (int secondIndex = 0; secondIndex < MAX_COLLIDERS; secondIndex++) {
+						if (colliders[secondLayer][secondIndex] != NULL) {
+							Collider * cdef = colliders[firstLayer][firstIndex];
+							Collider * catk = colliders[secondLayer][secondIndex];
+							Ellipse ellipse = catk->AttackArea;
+							ellipse.width += 2.0f * cdef->HitRadiusPlus;
+							ellipse.height += 2.0f * cdef->HitRadiusPlus;
+							if (ellipse_contains_scalar(&ellipse, cdef->AttackArea.x, cdef->AttackArea.y)) {
+								cdef->HitEvent(catk, SDL_TRUE);
+								catk->HitEvent(cdef, SDL_FALSE);
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
--- Game/GameContents/JudgingSystem.h
+++ Game/GameContents/JudgingSystem.h
@@ -1,3 +1,24 @@
 #ifndef JUDGING_SYSTEM_H
 #define JUDGING_SYSTEM_H
+#include "../SDLEx/SDLWithPlugins.h"
+#include "../SDLEx/MathEx/MathEx.h"
+#include "../SDLEx/Utils/Ellipse.h"
+#include "../SDLEx/Utils/Deque.h"
+#define COLLIDER_ACTION int
+#define COLLIDER_CLASSES 8
+
+typedef struct Collider {
+    Ellipse AttackArea;  /* Hit Others */
+	float HitRadiusPlus;  /* Being Hit */
+	float Damage;
+	float Defense;  /* Multiply with Damage Taken */
+	short Class;
+	short CollideMask;
+	unsigned _internalId;
+	COLLIDER_ACTION(*HitEvent)(struct Collider *, SDL_bool);  /* Another Collider; Is Hit */
+} Collider;
+
+void register_judge(Collider * collider);
+void unregister_judge(Collider * collider);
+void judge_hits();
 #endif
